d8354e5bef8661cc2209df962122843e5c31309c
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-05-12 15:29:25 +0200

    Adds documentation for the constants

--- Sources/Library/Constant.swift
+++ Sources/Library/Constant.swift
@@ -2,35 +2,103 @@ import UIKit
 
 public struct Constant {
 
+  /**
+   This is the font that will be displayed in the tab bar. For your controller's title.
+   */
   public struct Font {
+    /**
+     The title of the font.
+     */
     public static var title = UIFont.systemFontOfSize(11)
   }
 
+  /**
+   This represents multiple colors of the tab bar.
+   */
   public struct Color {
+    /**
+     The background of the tab bar.
+     */
     public static var background = UIColor.blackColor()
+
+    /**
+     The color of the disabled icon, the icon that is not tapped.
+     */
     public static var disabled = UIColor.lightGrayColor()
+
+    /**
+     The color of the enabled state.
+     */
     public static var enabled = UIColor.whiteColor()
+
+    /**
+     The color of the selected state.
+     */
     public static var selected = UIColor.whiteColor()
+
+    /**
+     The color of the separator between the tab bar and the controller.
+     */
     public static var separator = UIColor.lightGrayColor()
+
+    /**
+     The color of the indicator in the tab bar.
+     */
     public static var indicator = Color.selected
+
+    /**
+     The color of the shadow that will substitute the indicator.
+     */
     public static var shadow = UIColor.blackColor().colorWithAlphaComponent(0.1)
   }
 
+  /**
+   The dimensions of the tab bar.
+   */
   public struct Dimension {
+    /**
+     The with of the tab bar.
+     */
     public static var width = UIScreen.mainScreen().bounds.width
+
+    /**
+     The height of the tab bar.
+     */
     public static var height: CGFloat = 50
 
+    /**
+     This represents the dimensions of the indicator.
+     */
     public struct Indicator {
+      /**
+       The width of the indicator.
+       */
       public static var width: CGFloat = 25
+
+      /**
+       The height of the indicator.
+       */
       public static var height: CGFloat = 4
     }
 
+    /**
+     This represents the dimensions of the separator.
+     */
     public struct Separator {
+      /**
+       The height of the separator.
+       */
       public static var height: CGFloat = 0.25
     }
   }
 
+  /**
+   The default animation the tab bar will have.
+   */
   public struct Animation {
+    /**
+     The initial animation the tab bar will have.
+     */
     public static var initial: TabbyAnimation.Kind = .Pop
   }
 }
