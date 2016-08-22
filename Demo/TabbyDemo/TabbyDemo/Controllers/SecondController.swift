c9b644a414abaab05f83aa7a7848e7926d981446
Christoffer Winterkvist <christoffer@hyper.no>
2016-08-22 09:57:29 +0200

    Move setting the title to init

--- Demo/TabbyDemo/TabbyDemo/Controllers/SecondController.swift
+++ Demo/TabbyDemo/TabbyDemo/Controllers/SecondController.swift
@@ -2,6 +2,16 @@ import UIKit
 
 class SecondController: GeneralController {
 
+  override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+
+    title = "Donut".uppercaseString
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
--- Demo/TabbyDemo/TabbyDemo/Controllers/ThirdController.swift
+++ Demo/TabbyDemo/TabbyDemo/Controllers/ThirdController.swift
@@ -2,10 +2,19 @@ import UIKit
 
 class ThirdController: GeneralController {
 
+  override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+
+    title = "Fish".uppercaseString
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    title = "Fish".uppercaseString
     titleLabel.text = "Fish... Am I right?"
   }
 }
