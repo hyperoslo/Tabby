895e94a2503b0a362859f72389aa3bf08c4a762e
Khoa Pham <onmyway133@gmail.com>
2017-11-27 16:15:04 +0100

    Remove IndicatorPosition

--- Sources/Library/IndicatorPosition.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-/// `IndicatorPosition` is used in `TabbyBar` to configure where the indicator should be constrained.
-///
-/// - top: Constrainted to the top of the tab bar.
-/// - bottom: Constrainted to the bottom of the tab bar.
-public enum IndicatorPosition {
-  case top, bottom
-}
